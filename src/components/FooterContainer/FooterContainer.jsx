16d788b7dee38477faf44529aab1b216ea0c9f0c
Derrek Solitario <118510210+derrekdev@users.noreply.github.com>
2025-08-06 00:12:46 +0800

    added footer label and styles

--- src/components/FooterContainer/FooterContainer.jsx
+++ src/components/FooterContainer/FooterContainer.jsx
@@ -1,7 +1,14 @@
+import { FaGithub } from "react-icons/fa";
 export default function FooterContainer() {
   return (
     <footer>
-      <span>Footer</span>
+      <span>All rights reserve 2025</span>
+      {/* <span>test</span> */}
+      <span>
+        <a href="https://github.com/derrekdev/theme-palette" target="_blank">
+          <FaGithub size={18} /> Github Repo
+        </a>
+      </span>
     </footer>
   );
 }
